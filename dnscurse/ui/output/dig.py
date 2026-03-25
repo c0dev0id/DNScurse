1be1e19fa6bd515f49994bff6c858fd7e62e5e52
Marc 'Zugschlus' Haber <mh+github@zugschlus.de>
2026-03-25 10:20:12 +0100

    Zg (#11)
    * add UI module
    
    this adds an object oriented output mechanism allowing for
    different output formats. The "compact" and "short" outputs
    have been taken from the original code.
    
    Added:
    - dig (not yet implemented) to mimic dig -x output
    - pprint to pprint the raw python return value
    
    * remove helper functions
    
    they are now in ui.helpers
    
    * make pylint less unhappy
    
    * use new output class
    
    This also implements a new output option and changes the
    compact option, preserving backwards compatibililty. Feel free to
    ditch --compact completely for simplicity.
    
    ---------
    
    Co-authored-by: Marc Haber <mh+git-unconfigured-202508@zugschlus.de>

---
+++ dnscurse/ui/output/dig.py
@@ -0,0 +1,10 @@
+# dnscurse/ui/output/dig.py
+from .base import Outputter
+
+class DigOutputter(Outputter):
+    def _output_steps(self) -> int:
+        # implement dig-style output here
+        print("this is not yet implemented")
+        for step in self.steps:
+            print(f"; {step.query_name} {step.query_type} {step.response}", file=self.out)
+        return 0
